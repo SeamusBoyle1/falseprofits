SeamusBoyle1/falseprofits

commit 8040c7627c79fb3471eb2056d7d89991ebc7ff98

Fix Infinite% PnL value displayed in portfolio
Caused by the API returning an average share purchase price (AKA
averagePrice) of zero.

src/FalseProfitsMobile/PortfolioListView.qml
@@ -118,8 +118,9 @@ ListView {
 
                 Label {
                     id: profitLossLabel
-                    text: qsTr("PnL: %1 (%2%)").arg(model.profitLoss.toFixed(0)).arg(
-                              model.profitLossPercent.toFixed(1))
+                    text: model.profitLossPercent && model.profitLoss ?
+                              qsTr("PnL: %1 (%2%)").arg(model.profitLoss.toFixed(0)).arg(
+                                  model.profitLossPercent.toFixed(1)) : ""
                     // TODO(seamus): Use FpStyle for color
                     color: model.direction === 1 ? "#0b893e" : model.direction
                                                    === -1 ? "#bf1722" : "black"

src/FalseProfitsCore/fppositionslistmodel.cpp
@@ -88,7 +88,9 @@ QVariant FpPositionsListModel::data(const QModelIndex &index, int role) const
         }
         case ProfitLossPercentRole: {
             auto const &e = m_rows.at(row);
-            if (!(e.lastPrice > 0))
+            // average price must be non-zero otherwise cost is zero
+            // which causes a divide by zero error
+            if (!(e.lastPrice > 0 && e.averagePrice > 0))
                 return QVariant();
             auto marketValue = e.quantity * e.lastPrice;
             auto cost = e.quantity * e.averagePrice;

commit e1307cf44c41f2e521ba89f2f72fad052110392c

Fix display of zero profit
This bug was introduced in 8040c7627c79fb3471eb2056d7d89991ebc7ff98

src/FalseProfitsMobile/PortfolioListView.qml
@@ -118,7 +118,7 @@ ListView {
 
                 Label {
                     id: profitLossLabel
-                    text: model.profitLossPercent && model.profitLoss ?
+                    text: model.profitLossPercent !== undefined && model.profitLoss !== undefined ?
                               qsTr("PnL: %1 (%2%)").arg(model.profitLoss.toFixed(0)).arg(
                                   model.profitLossPercent.toFixed(1)) : ""
                     // TODO(seamus): Use FpStyle for color