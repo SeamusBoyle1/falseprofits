SeamusBoyle1/falseprofits

commit 2d5815edc3c107f0216ec8e572d3ebad72d6c1ca

Workaround for incorrect number values on iOS
The Light weighted font used on iOS doesn't doesn't display numbers
properly. Until Roboto font is also being used on iOS light weight can't
be used.

src/FalseProfitsMobile/DetailedQuotePageForm.ui.qml
@@ -177,8 +177,14 @@ Page {
 
                             Label {
                                 id: lastPriceLabel
-                                font.pixelSize: 45
-                                font.weight: Font.Light
+                                // Workaround for Light weighted font not displaying
+                                // correct values on iOS. The UI design had this label at
+                                // size 45sp with Light weight, this workaround reduces the
+                                // font size and uses default (probably Normal) weight.
+                                // TODO(seams): Change pixelSize back to 45 and font.weight
+                                // back to Light when Roboto font is used on iOS.
+                                font.pixelSize: 34
+                                //font.weight: Font.Light
                                 Layout.alignment: Qt.AlignHCenter | Qt.AlignTop
                             }
 

commit ef8592ca57c36d96b62a09de5341ee37d29630b7

Fix change values alignment with last price
This layout bug was introduced in
2d5815edc3c107f0216ec8e572d3ebad72d6c1ca

src/FalseProfitsMobile/DetailedQuotePageForm.ui.qml
@@ -185,7 +185,6 @@ Page {
                                 // back to Light when Roboto font is used on iOS.
                                 font.pixelSize: 34
                                 //font.weight: Font.Light
-                                Layout.alignment: Qt.AlignHCenter | Qt.AlignTop
                             }
 
                             Label {
@@ -195,8 +194,7 @@ Page {
                                                         == -1 ? "#bf1722" :
                                                                 ExtraMaterial.primaryTextColor
                                 font.pixelSize: 32
-                                Layout.alignment: Qt.AlignHCenter | Qt.AlignBottom
-                                Layout.bottomMargin: 7
+                                anchors.baseline: lastPriceLabel.baseline
                             }
 
                             Label {
@@ -206,8 +204,7 @@ Page {
                                 color: direction == 1 ? "#0b893e" : direction
                                                         == -1 ? "#bf1722" :
                                                                 ExtraMaterial.primaryTextColor
-                                Layout.alignment: Qt.AlignHCenter | Qt.AlignBottom
-                                Layout.bottomMargin: 7
+                                anchors.baseline: lastPriceLabel.baseline
                             }
 
                             Label {
@@ -218,8 +215,7 @@ Page {
                                                         == -1 ? "#bf1722" :
                                                                 ExtraMaterial.primaryTextColor
                                 horizontalAlignment: Text.AlignRight
-                                Layout.alignment: Qt.AlignHCenter | Qt.AlignBottom
-                                Layout.bottomMargin: 7
+                                anchors.baseline: lastPriceLabel.baseline
                             }
                         }
 