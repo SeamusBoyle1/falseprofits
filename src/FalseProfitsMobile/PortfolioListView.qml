SeamusBoyle1/falseprofits

commit 69c94a9b2face1e2ff7543deb8fc4acacfad129f

Fix pressed state color of swipe hidden actions
...in Portfolio and Watchlist lists.

src/FalseProfitsMobile/PortfolioListView.qml
@@ -63,6 +63,7 @@ ListView {
                         var qty = listView.model.getQuantityForRow(index)
                         marketOrderTriggered(symbol, qty)
                     }
+                    onPressed: mouse.accepted = false
                 }
             }
         }

src/FalseProfitsMobile/WatchlistListView.qml
@@ -61,6 +61,7 @@ ListView {
                         listView.model.remove(index)
                         triggerRemoveSymbol(symbol)
                     }
+                    onPressed: mouse.accepted = false
                 }
             }
 

commit 9393d6d5e2ce3883401af1eeab23607f65c8bc22

Fix swipe hidden actions on Portfolio, Watchlist
The actions are "Close Position" on the Portfolio page, and "Delete"
watchlist item on the Watchlist page.

This was broken in 69c94a9b2face1e2ff7543deb8fc4acacfad129f "Fix pressed
state color of swipe hidden actions".

Using a MouseArea instead of SwipeDelegate.onClicked is a workaround for
Android. This commit fixes that workaround and still correctly shows the
pressed state.

src/FalseProfitsMobile/PortfolioListView.qml
@@ -55,16 +55,16 @@ ListView {
 
             background: Rectangle {
                 // TODO(seamus): Use FpStyle for color
-                color: closePositionLabel.SwipeDelegate.pressed ? Qt.darker("tomato", 1.1) :
-                                                                  "tomato"
+                color: closePositionLabelMouseArea.pressed ? Qt.darker("tomato", 1.1) : "tomato"
+
                 MouseArea {
+                    id: closePositionLabelMouseArea
                     anchors.fill: parent
                     onClicked: {
                         var symbol = listView.model.getSymbolForRow(index)
                         var qty = listView.model.getQuantityForRow(index)
                         marketOrderTriggered(symbol, qty)
                     }
-                    onPressed: mouse.accepted = false
                 }
             }
         }

src/FalseProfitsMobile/WatchlistListView.qml
@@ -54,8 +54,10 @@ ListView {
             Rectangle {
                 anchors.fill: parent
                 // TODO(seamus): Use FpStyle for color
-                color: deleteLabel.SwipeDelegate.pressed ? Qt.darker("tomato", 1.1) : "tomato"
+                color: deleteLabelMouseArea.pressed ? Qt.darker("tomato", 1.1) : "tomato"
+
                 MouseArea {
+                    id: deleteLabelMouseArea
                     anchors.fill: parent
                     onClicked: {
                         // Optimistic removal
@@ -63,7 +65,6 @@ ListView {
                         listView.model.remove(index)
                         triggerRemoveSymbol(symbol)
                     }
-                    onPressed: mouse.accepted = false
                 }
             }
 